fa8e58837269e4913adc22bfb341e38d663de37e
CoderButNoob <sonaraniket20@gmail.com>
2025-04-16 23:06:12 +0530

    UC3-Edit Contact

--- address_book.py
+++ address_book.py
@@ -32,4 +32,31 @@ class AddressBook:
         print("\n All Contacts")
         for i , detail in enumerate(self.details,1):
             print("\n Contact{i} : {detail}")
+    
+    def edit_contacts(self):
+        name = input("Enter the first name of contact to edit")
+        for detail in self.details:
+            if detail.first_name.lower() == name.lower():
+                print("Edit Data or Press Enter to keep the existing data...")
+
+                updated_data = {
+                    "first_name" : detail.first_name,
+                    "last_name" : input("Edit Last Name [{detail.last_name}]: ") or detail.last_name,
+                    "address" : input("Edit Address [{detail.address}]: ") or detail.address,
+                    "city" : input("Edit City [{detail.city}]: ") or detail.city,
+                    "state" : input("Edit state [{detail.state}]: ") or detail.state,
+                    "zip_code" : input("Edit zip_code [{detail.zip_code}]: ") or detail.zip_code,
+                    "phone" : input("Edit phone [{detail.phone}]: ") or detail.phone,
+                    "email" : input("Edit email [{detail.email}]: ") or detail.email,                     
+                }
+            try:
+                updated_data = Details(**updated_data)
+                self.details[self.details.index(detail)] = updated_data
+                print("Contact Updated")
+            except Exception as e:
+                print("Validation error" , e)
+            return
+        print("Contact Not Found")
+        
+
 
